311ebead478510e57a5a561a9449c840b8edc0e3
abdelhalem.salah@gmail.com <abdelhalem.salah@gmail.com>
2019-01-21 13:56:38 +0200

    remove websocket

--- chequeClearing/src/main/java/com/egabi/blockchain/chequeClearing/ChequeClearingApplication.java
+++ chequeClearing/src/main/java/com/egabi/blockchain/chequeClearing/ChequeClearingApplication.java
@@ -1,6 +1,6 @@
 package com.egabi.blockchain.chequeClearing;
 
-import com.egabi.blockchain.chequeClearing.notification.WebSocketNotificationClient;
+//import com.egabi.blockchain.chequeClearing.notification.WebSocketNotificationClient;
 //import com.egabi.blockchain.chequeClearing.notification.WebSocketNotificationClient;
 import com.egabi.blockchain.chequeClearing.utils.StorageProperties;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,20 +18,20 @@ import org.springframework.scheduling.annotation.Scheduled;
 public class ChequeClearingApplication {
 
 
-    @Autowired
-    WebSocketNotificationClient webSocketNotificationClient;
+//    @Autowired
+//    WebSocketNotificationClient webSocketNotificationClient;
 
 
     public static void main(String[] args) {
         SpringApplication.run(ChequeClearingApplication.class, args);
     
     }
-    @Scheduled(fixedRate = 10)
-    public void reminder() {
+//    @Scheduled(fixedRate = 10)
+//    public void reminder() {
+//
+//        webSocketNotificationClient.sendNotificationToUser("hello adry");
+//
+//    
 
-        webSocketNotificationClient.sendNotificationToUser("hello adry");
 
-    
-
-}
 }
